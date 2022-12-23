39c5a2b8f9fbd0cb9ba78cf4169d91d768ddf87a
Jonah <jonahcole@yahoo.com>
2022-12-23 12:29:09 -0800

    Extract all route logic code into its own class while keeping all routes in Program.cs

---
+++ MethodsAPI.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BooksAPI
+{
+    public class MethodsAPI
+    {
+
+        public static async Task<List<Book>> GetAllBooks(BooksContext db)
+        {
+            return await db.Books.ToListAsync();
+        }
+
+        public static async Task<IResult> GetBookById (int id, BooksContext db)
+        {
+            var book = await db.Books.FindAsync(id);
+
+            return book != null ? Results.Ok(book) : Results.NotFound();
+        }
+
+        public static async Task<IResult> PostBook(Book book, BooksContext db)
+        {
+            db.Books.Add(book);
+            await db.SaveChangesAsync();
+
+            return Results.Created($"/books/{book.Id}", book);
+        }
+
+        public static async Task<IResult> DeleteBook(int id, BooksContext db)
+        {
+            var book = await db.Books.FindAsync(id);
+            if (book != null)
+            {
+                db.Books.Remove(book);
+                db.SaveChanges();
+
+                return Results.NoContent();
+            }
+            else
+            {
+                return Results.NotFound();
+            }
+        }
+
+        public static async Task<IResult> UpdateBook(int id, Book book, BooksContext db)
+        {
+            //could also use db.Books.Update(book); and wouldnt need following line and all the manual updating below
+            var bookInDb = await db.Books.FindAsync(id);
+
+            if (bookInDb != null)
+            {
+                //Updating each property manually
+                //could also only update the needed ones
+                bookInDb.Title = book.Title;
+                bookInDb.Author = book.Author;
+                bookInDb.Genre = book.Genre;
+                bookInDb.Publisher = book.Publisher;
+                bookInDb.PublicationDate = book.PublicationDate;
+                db.SaveChanges();
+
+                return Results.Ok(bookInDb);
+            }
+            else
+            {
+                return Results.NotFound();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,61 +13,11 @@ builder.Services.AddDbContext<BooksContext>(options => options.UseInMemoryDataba
 var app = builder.Build();
 
 //adding endpoints
-app.MapGet("/books", async (BooksContext db) => await db.Books.ToListAsync());
-app.MapGet("/books/{id}", async (int id, BooksContext db) =>
-{
-    var book = await db.Books.FindAsync(id);
-
-    return book != null ? Results.Ok(book) : Results.NotFound();
-
-});
-app.MapPost("/books", async (Book book, BooksContext db) =>
-{
-    db.Books.Add(book);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/books/{book.Id}", book);
-});
-app.MapDelete("/books/{id}", async (int id, BooksContext db) =>
-{
-    var book = await db.Books.FindAsync(id);
-    if (book != null)
-    {
-        db.Books.Remove(book);
-        db.SaveChanges();
-
-        return Results.NoContent();
-    }
-    else
-    {
-        return Results.NotFound();
-    }
-});
-app.MapPut("/books/{id}", async (int id, Book book, BooksContext db) =>
-{
-    //could also use db.Books.Update(book); and wouldnt need following line and all the manual updating below
-    var bookInDb = await db.Books.FindAsync(id);
-
-    if (bookInDb != null)
-    {
-        //Updating each property manually
-        //could also only update the needed ones
-        bookInDb.Title = book.Title;
-        bookInDb.Author = book.Author;
-        bookInDb.Genre = book.Genre;
-        bookInDb.Publisher = book.Publisher;
-        bookInDb.PublicationDate = book.PublicationDate;
-        db.SaveChanges();
-
-        return Results.Ok(bookInDb);
-    }
-    else
-    {
-        return Results.NotFound();
-    }
-
-});
-
+app.MapGet("/books", async (BooksContext db) => await MethodsAPI.GetAllBooks(db));
+app.MapGet("/books/{id}", async (int id, BooksContext db) => await MethodsAPI.GetBookById(id, db));
+app.MapPost("/books", async (Book book, BooksContext db) => await MethodsAPI.PostBook(book, db));
+app.MapDelete("/books/{id}", async (int id, BooksContext db) => await MethodsAPI.DeleteBook(id, db));
+app.MapPut("/books/{id}", async (int id, Book book, BooksContext db) => await MethodsAPI.UpdateBook(id, book, db));
 
 if (app.Environment.IsDevelopment())
 {
